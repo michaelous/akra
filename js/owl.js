aebc83a0625cb43a0c67722ca91d491d2f411796
michaelous <m.kapala@getprintbox.com>
2019-11-28 22:09:44 +0100

    add company section with another owl-carousel

--- js/owl.js
+++ js/owl.js
@@ -1,6 +1,5 @@
 $(function() {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
+    $(".owl-one").owlCarousel({
         merge: true,
         loop: true,
         margin: 10,
@@ -19,4 +18,43 @@ $(function() {
             }
         }
     });
+
+    $(".owl-three").owlCarousel({
+        merge: true,
+        loop: true,
+        margin: 10,
+        video: true,
+        lazyLoad: true,
+        center: true,
+        responsive: {
+            320: {
+                items: 1
+            },
+            560: {
+                items: 2
+            },
+            992: {
+                items: 1
+            }
+        }
+    });
+
+});
+
+
+
+$(".owl-two").owlCarousel({
+    loop: true,
+    responsive: {
+        300: {
+            items: 3,
+        },
+        400: {
+            items: 3,
+        },
+
+        800: {
+            items: 4,
+        },
+    }
 });
