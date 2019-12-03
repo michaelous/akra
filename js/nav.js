89fa5f8f167cc52bf5ff54f5eaf8fa1391afdf6f
michaelous <m.kapala@getprintbox.com>
2019-12-03 20:48:57 +0100

    svg arrows with keyframes animation in first section

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,16 @@
 window.addEventListener('scroll', function() {
     var nav = document.querySelector(".nav-container");
+    var scrollArrows = document.querySelector('.scroll-arrows');
     let scroll = window.scrollY;
     if (scroll > 100) {
-        nav.classList.add('nav-scroll')
+        {
+            nav.classList.add('nav-scroll')
+            scrollArrows.classList.remove('arrows-visible');
+        }
+
     } else {
         nav.classList.remove('nav-scroll');
+        scrollArrows.classList.add('arrows-visible');
+
     }
 });
-const numbers = document.querySelectorAll('.js-count');
-for (i = 0; i < numbers.length; ++i) {
-    let val = numbers[i].innerHTML;
-
-};
